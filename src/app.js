51f98ef820a77a456764e377deb0ed23f995849c
lilei <tothis@qq.com>
2020-03-11 14:57:13 +0800

    initial

--- src/app.js
+++ src/app.js
@@ -3,24 +3,24 @@ import logo from './logo.svg';
 import './app.css';
 
 function app() {
-  return (
-    <div className="app">
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <header className="app-header">
+                <img src={logo} className="app-logo" alt="logo"/>
+                <p>
+                    edit <code>src/app.js</code> and save to reload.
+                </p>
+                <a
+                    className="app-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    learn react
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default app;
