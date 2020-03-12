3c298e4a89c2839e717e41437eab0b778181d2cc
lilei <tothis@qq.com>
2020-03-12 10:44:28 +0800

    initial

--- src/router.js
+++ src/router.js
@@ -1,16 +1,17 @@
 // 使用react jsx写法必引
-import React from 'react';
+import React from 'react'
 // 使用BrowserRouter路由模式 http://react-guide.github.io/react-router-cn/docs/guides/basics/Histories.html
-import {HashRouter, Route} from "react-router-dom";
+import {HashRouter, Route} from "react-router-dom"
 // loadable
-import load from "./util/load";
+import load from "./util/load"
 // 组件
-import index from './view/index';
-// import item from './view/item';
-const item = load(() => import ('./view/item'));
+import index from './view/index'
+// import item from './view/item'
+const item = load(() => import ('./view/item'))
 
 export default () => (
     <HashRouter>
         <Route path="/" component={index}/>
         <Route path="/item" component={item}/>
-    </HashRouter>);
+    </HashRouter>
+)
--- src/view/item/index.jsx
+++ src/view/item/index.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
-import './index.css';
+import React from 'react'
+import './index.css'
 
-class item extends React.Component {
+class Father extends React.Component {
+
+    state = {
+        sonMessage: null
+    }
+
+    message = '父组件数据'
+
+    sonMessage = param => {
+        // 子组件向父组件传值必须使用setState修改值 否则父组件不会重新渲染
+        this.setState({
+            sonMessage: param
+        })
+    }
+
     render() {
-        let text = <p>item</p>;
         return (
-            <div>{text}</div>
-        );
+            <div>
+                <p>子组件数据[{this.state.sonMessage}]</p>
+                <Son father={this} cancel={mode => this.cancel(mode)}/>
+            </div>
+        )
     }
 }
 
-export default item;
+// 子组件对父组件传值 需调用父组件方法
+class Son extends React.Component {
+    message = '子组件数据'
+
+    constructor(props) {
+        super(props)
+        this.props.father.sonMessage(this.message)
+    }
+
+    render() {
+        return (
+            <p>
+                父组件数据[{this.props.father.message}]
+            </p>
+        )
+    }
+}
+
+export default Father
