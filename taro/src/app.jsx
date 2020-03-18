f8d38c0da18efc988150d7239b8bd1ea17ddf542
lilei <tothis@qq.com>
2020-03-18 20:01:54 +0800

    initial

---
+++ taro/src/app.jsx
@@ -0,0 +1,45 @@
+import Taro, {Component} from '@tarojs/taro'
+import Index from './pages/index'
+
+import './app.styl'
+
+// 如需在h5环境开启React Devtools
+// 取消以下注释
+// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
+//   require('nerv-devtools')
+// }
+
+class App extends Component {
+
+    config = {
+        pages: [
+            'pages/index/index'
+        ],
+        window: {
+            backgroundTextStyle: 'light',
+            navigationBarBackgroundColor: '#fff',
+            navigationBarTitleText: 'WeChat',
+            navigationBarTextStyle: 'black'
+        }
+    }
+
+    componentDidMount() {
+    }
+
+    componentDidShow() {
+    }
+
+    componentDidHide() {
+    }
+
+    componentDidCatchError() {
+    }
+
+    render() {
+        return (
+            <Index/>
+        )
+    }
+}
+
+Taro.render(<App/>, document.getElementById('app'))
